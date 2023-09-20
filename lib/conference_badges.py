d93d6bc1d070e00515f5f22123d6bbb1799b6254
Julie C <jucheng925@gmail.com>
2023-09-20 14:58:09 -0400

    use list comphrension to refactor functions

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    # batch_list = list()
+    # for name in names:
+    #     batch_list.append(badge_maker(name))
+    batch_list = [badge_maker(name) for name in names]
+    return batch_list
 
 def assign_rooms(names):
-    return None
+    assign_list = list()
+    # for index in range(len(names)):
+    #     message = f'Hello, {names[index]}! You\'ll be assigned to room {index + 1}!'
+    #     assign_list.append(message)
+    assign_list = [f'Hello, {names[index]}! You\'ll be assigned to room {index + 1}!' for index in range(len(names))]
+    return assign_list
 
 def printer(names):
-    return None
+    for batch in batch_badge_creator(names):
+        print(batch)
+    for assign in assign_rooms(names):
+        print(assign)
